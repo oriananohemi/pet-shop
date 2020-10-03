4dd81ae3b5faae4435471fb1801bbecd186aacf5
Oriana <oriana.nohemi22@gmail.com>
2020-10-03 16:14:18 -0500

    implementar el home como un modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,9 @@ import { ProductComponent } from './components/product/product.component';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
 import { ProductsComponent } from './components/products/products.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { DemoComponent } from './components/demo/demo.component';
-import { BannerComponent } from './components/banner/banner.component';
 import { CardComponent } from './components/card/card.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
@@ -24,11 +22,9 @@ import { LayoutComponent } from './components/layout/layout.component';
     ProductComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent,
     ProductsComponent,
     ContactComponent,
     DemoComponent,
-    BannerComponent,
     CardComponent,
     NotfoundComponent,
     ProductDetailComponent,
