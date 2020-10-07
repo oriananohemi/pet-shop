3e0e807432f0704f39f1da5551ab17ac5d96da03
Oriana <oriana.nohemi22@gmail.com>
2020-10-07 16:33:11 -0500

    agregar productos al carrito de compras

--- src/app/products/components/product/product.component.ts
+++ src/app/products/components/product/product.component.ts
@@ -1,20 +1,24 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../../../core/definitions/product.model';
 
+import { CartService } from '../../../core/services/cart/cart.service';
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
-
 export class ProductComponent {
   @Input() product: Product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
 
   today = new Date();
 
+  constructor(private cartService: CartService) {}
+
   addCart() {
     console.log('Agregar al carrito');
-    this.productClicked.emit(this.product.id);
+    // this.productClicked.emit(this.product.id);
+    this.cartService.addCart(this.product);
   }
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from '../../../core/services/cart/cart.service';
+
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  // total = 0;
+
+  total$: Observable<number>;
+
   logo = 'assets/images/logo.png';
 
-  constructor() { }
+  // constructor(
+  //   private cartService: CartService
+  // ) {
+  //   this.cartService.cart$.subscribe(products => {
+  //     console.log(products);
+  //     this.total = products.length;
+  //   });
+  //  }
+
+  constructor(
+    private cartService: CartService
+  ) {
+    this.total$ = this.cartService.cart$
+    .pipe(
+      map(products => products.length));
+   }
 
   ngOnInit(): void {
   }
